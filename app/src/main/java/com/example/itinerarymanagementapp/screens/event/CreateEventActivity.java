c54340aa43ddedd68772aa1c187361c69d012bee
Gael Estrera <gaelestrera@gmail.com>
2023-11-20 23:07:07 +0800

    Feat: Initialized code structure and added empty views for Events

---
+++ app/src/main/java/com/example/itinerarymanagementapp/screens/event/CreateEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.itinerarymanagementapp.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itinerarymanagementapp.R;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
---
+++ app/src/main/java/com/example/itinerarymanagementapp/screens/event/EditEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.itinerarymanagementapp.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itinerarymanagementapp.R;
+
+public class EditEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_event);
+    }
+}

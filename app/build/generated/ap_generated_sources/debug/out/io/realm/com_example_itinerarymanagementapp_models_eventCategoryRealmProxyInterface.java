f9306bd4b9f2ed0c56c326172a1c628683688ec6
Gael Estrera <gaelestrera@gmail.com>
2023-12-02 01:52:46 +0800

    Gael Feat: Removed Build from Git tracking

--- app/build/generated/ap_generated_sources/debug/out/io/realm/com_example_itinerarymanagementapp_models_eventCategoryRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface com_example_itinerarymanagementapp_models_eventCategoryRealmProxyInterface {
-    public String realmGet$uuid();
-    public void realmSet$uuid(String value);
-    public String realmGet$name();
-    public void realmSet$name(String value);
-}
